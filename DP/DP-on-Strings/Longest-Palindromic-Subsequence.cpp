c9f8e8a5741810d8b94bb3caae32d9ba926bd908
Tushar Bhart <121730758+TusharBhart@users.noreply.github.com>
2023-01-12 16:46:34 +0530

    Add files via upload

---
+++ DP/DP-on-Strings/Longest-Palindromic-Subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    int dfs(int i, int j, string &s, string &t, vector<vector<int>> &dp) {
+        if(i < 0 || j < 0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s[i] == t[j]) return dp[i][j] = 1 + dfs(i - 1, j - 1, s, t, dp);
+        return dp[i][j] = max(dfs(i - 1, j, s, t, dp), dfs(i, j - 1, s, t, dp));
+    }
+public:
+    int longestPalindromeSubseq(string s) {
+        string t = s;
+        reverse(t.begin(), t.end());
+
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        return dfs(n - 1, n - 1, s, t, dp);
+    }
+};
